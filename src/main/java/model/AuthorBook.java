31ed23cd470e2d078952ea59cd7f5451c39fd9b6
anhb2012060 <anhb2012060@gmail.com>
2023-11-02 22:15:36 +0700

    Lớp model Author Book

---
+++ src/main/java/model/AuthorBook.java
@@ -0,0 +1,17 @@
+/***********************************************************************
+ * Module:  AuthorBook.java
+ * Author:  trand
+ * Purpose: Defines the Class AuthorBook
+ ***********************************************************************/
+
+import java.util.*;
+
+/** @pdOid 90e57cfa-2046-4cfe-b3cd-6b8a091829ba */
+public class AuthorBook {
+   /** @pdOid 1d6e4907-4ac8-4624-a1a9-48bd220a3edc */
+   private String bookID;
+   /** @pdOid 93e01dd6-b241-4259-b25f-37361b74f89e */
+   private String authorID;
+   
+   
+}
